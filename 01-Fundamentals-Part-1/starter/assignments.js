429fb64366eaaba38ddd3d707b1e324ddaf6cd27
Bryan <37481349+bryare@users.noreply.github.com>
2022-08-08 17:30:15 -0500

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,20 @@
+// LECTURE: Values and Variables
+
+/* let country = "USA";
+let continent = "North America";
+let population = 332403650;
+
+console.log(country + " " + continent + " " + population);
+ */
+
+// LECTURE: Data Types
+
+/* let isIsland = false;
+let country = "USA";
+let population = 332403650;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language); */
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,41 @@
+// Linking a JavaScript File
+
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10); */
+
+// Values and Variables
+
+/* console.log("Jonas");
+console.log(23);
+
+let firstName = "Bryan";
+console.log(firstName);
+
+let jonas_matildas = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415; */
+
+// Data Types
+
+/* true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Bryan");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); */
