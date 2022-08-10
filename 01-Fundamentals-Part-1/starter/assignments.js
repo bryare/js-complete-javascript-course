d04e732816aa8ae18a613112112b462a7385bd65
Bryan <37481349+bryare@users.noreply.github.com>
2022-08-10 15:58:31 -0500

    add switch statement lecture and assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -96,3 +96,29 @@ if (countrySpeaksEnglish && hasLessThanFiftyMillionPop && isAnIsland) {
 } else {
   console.log("America does not meet your criteria.");
 } */
+
+// LECTURE: The switch Statement
+
+/* const language = "english";
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("Most number of native speakers");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too");
+    break;
+} */
