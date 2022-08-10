061064318429a8c125581686398feca265b72bad
Bryan <37481349+bryare@users.noreply.github.com>
2022-08-10 14:10:09 -0500

    add LECTURE: Equality Operators: == vs ===

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -191,3 +191,26 @@ if (height) {
 } else {
   console.log("Height is undefined"); // height is not actually undefined, its 0, so this is a bug
 } */
+
+// Equality Operators: == vs ===
+
+/* const age = "18";
+if (age === 18) console.log("You just became an adult (strict)");
+if (age == 18) console.log("You just became an adult (loose)");
+
+const favorite = Number(prompt("What's your favorite number?"));
+
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  console.log("Cool! 23 is an amazing number");
+} else if (favorite === 7) {
+  console.log("7 is also a cool number");
+} else if (favorite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favorite !== 23) console.log("Why not 23"); */
