e06614ef168bca55ff1619af74356b10c6099e7d
Bryan <37481349+bryare@users.noreply.github.com>
2022-08-09 19:24:48 -0500

    add coding challenge 1 and 2

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -18,3 +18,45 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language); */
+
+// LECTURE: let, const, and var
+
+/* let language = "English";
+const country = "USA";
+const population = 332403650; */
+
+// LECTURE: Basic Operators
+
+/* 
+let country = "USA";
+const population = 332403650;
+const halfPopulation = population / 2;
+
+population++;
+
+const finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+console.log(population > 33000000);
+const description =
+  "US is in America, and its 322 million people speak English."; */
+
+// LECTURE: Strings and Template Literals
+
+/* const country = "USA";
+const population = 332;
+const continent = "North America";
+const language = "English";
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(description); */
+
+// LECTURE: Taking Decisions: if / else Statements
+
+const countryPopulation = 332;
+if (countryPopulation > 33) {
+  console.log(`USA's population is above average`);
+} else {
+  console.log(
+    `USA's population is ${33 - countryPopulation} million below average`
+  );
+}
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -39,3 +39,108 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null); */
+
+// let, const, and var
+
+/* let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName); */
+
+// Basic Operators
+
+/* const dateNow = 2037;
+const ageBryan = dateNow - 1997;
+const ageSarah = dateNow - 2018;
+console.log(ageBryan, ageSarah);
+
+// Math Operators
+console.log(ageBryan * 2, ageBryan / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Bryan";
+const lastName = "Arevalo";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10;
+x *= 4; // x = x * 4;
+x++;
+x--;
+console.log(x);
+
+//Comparison Operators
+
+console.log(ageBryan > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(dateNow - 1991 > dateNow - 2018); */
+
+// Operator Precedence
+
+/* const dateNow = 2037;
+const ageBryan = dateNow - 1997;
+const ageSarah = dateNow - 2018;
+console.log(dateNow - 1991 > dateNow - 2018);
+
+let x, y;
+
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageBryan + ageSarah) / 2;
+console.log(ageBryan, ageSarah, averageAge); */
+
+// Strings and Template Literals
+
+/* const firstName = "Bryan";
+const job = "programmer";
+const birthYear = 1997;
+const year = 2037;
+const bryan =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job;
+console.log(bryan);
+
+const bryanNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}.`;
+console.log(bryanNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`); */
+
+// Taking Decisions: if / else Statements
+
+/* const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years`);
+}
+
+const birthYear = 1997;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+} */
